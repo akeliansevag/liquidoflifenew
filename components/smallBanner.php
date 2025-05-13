9bec944b3653980d6bc8872eebb497e320ad593f
akeliansevag <akelian.sevag@gmail.com>
2025-05-13 22:18:27 +0300

    .

---
+++ components/smallBanner.php
@@ -0,0 +1,12 @@
+<?php if (get_field('banner_image')): ?>
+    <?php $image = get_field('banner_image'); ?>
+    <div class="relative max-lg:min-h-[50vh] lg:min-h-[60vh] w-full flex items-center">
+        <div class="overlay"></div>
+        <img class="-z-10 absolute object-cover top-0 left-0 w-full h-full" src="<?= $image['url'] ?>" alt="<?= $image['alt'] ?>" />
+        <?php if (get_field('banner_title')): ?>
+            <div class="container">
+                <h1 class="font-bold max-lg:text-3xl lg:text-7xl <?= get_field('white_font') ? 'text-white' : 'text-black' ?>"><?= nl2br(get_field('banner_title')) ?></h1>
+            </div>
+        <?php endif; ?>
+    </div>
+<?php endif; ?>
--- page-faqs.php
+++ page-faqs.php
@@ -5,15 +5,15 @@ $faq_categories = get_terms([
     'hide_empty' => true, // Set to false if you want to include empty categories
 ]);
 ?>
-
+<?= get_template_part('/components/smallBanner'); ?>
 <section>
     <div class="container">
-        <h1 class="section-title text-center"><?= the_title(); ?></h1>
+        <h1 class="section-title"><?= the_title(); ?></h1>
         <div class="flex flex-col gap-3 my-10">
             <?php foreach ($faq_categories as $c): ?>
-                <div class="bg-secondary rounded-3xl max-lg:p-4 lg:p-8">
-                    <h2 class="text-3xl font-bold text-[#676767] pl-3"><?= $c->name ?></h2>
-                    <div class="flex flex-col gap-4 mt-5">
+                <div class="max-lg:p-4 lg:p-8">
+                    <h2 class="text-3xl font-bold"><?= $c->name ?></h2>
+                    <div class="flex flex-col mt-5">
                         <?php
                         $faq_query = new WP_Query([
                             'post_type'      => 'faq',        // Custom post type
@@ -30,15 +30,15 @@ $faq_categories = get_terms([
                         ?>
                         <?php foreach ($questions as $q): ?>
                             <!-- FAQ Item -->
-                            <div class="bg-white rounded-xl border border-[#D8D8D8] overflow-hidden">
-                                <button class="faq-question w-full max-lg:px-3 lg:px-6 max-lg:py-3 lg:py-4 text-left flex justify-between items-center">
-                                    <span class="text-lg text-gray-700"><?= $q->post_title ?></span>
+                            <div class="bg-white border-b border-b-[#D8D8D8] overflow-hidden">
+                                <button class="faq-question w-full max-lg:pr-3 lg:pr-6 max-lg:py-4 lg:py-5 text-left flex justify-between items-center">
+                                    <span class="text-lg font-bold"><?= $q->post_title ?></span>
                                     <svg class="faq-icon shrink-0 w-5 h-5 text-gray-500 transform transition-transform duration-200" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
                                     </svg>
                                 </button>
                                 <div class="faq-answer">
-                                    <div class="max-lg:px-3 lg:px-6 pb-4 pt-2">
+                                    <div class="pb-4 pt-2">
                                         <?= $q->post_content ?>
                                     </div>
                                 </div>
