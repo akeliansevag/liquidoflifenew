82a8f1a2a18c5ab8e4debea1ae5dcc1856a48cc1
akeliansevag <akelian.sevag@gmail.com>
2025-05-15 00:25:21 +0300

    .

--- front-page.php
+++ front-page.php
@@ -3,6 +3,6 @@
 <?= get_template_part('components/aboutUs'); ?>
 <?= get_template_part('components/productsSlider'); ?>
 <?= get_template_part('components/clientsAndTestimonials'); ?>
-<?= get_template_part('components/FAQ'); ?>
+<?= get_template_part('components/faq'); ?>
 <?= get_template_part('components/certifications'); ?>
 <?php get_footer(); ?>
