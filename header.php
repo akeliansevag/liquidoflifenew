460ccac31b576e63f61c229010a4aaeff42ca301
akeliansevag <akelian.sevag@gmail.com>
2025-08-19 14:35:03 +0300

    .

--- header.php
+++ header.php
@@ -43,6 +43,18 @@
 			phoneNumber: "971588574919"
 		});
 	</script>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17391537004"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'AW-17391537004');
+	</script>
 	<?php wp_head(); ?>
 </head>
 
