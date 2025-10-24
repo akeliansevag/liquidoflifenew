e03273f9b0c7d117b9183e0c876cb5618a2b7e29
akeliansevag <akelian.sevag@gmail.com>
2025-10-24 12:03:24 +0300

    .

--- header.php
+++ header.php
@@ -82,6 +82,19 @@
 	<noscript><img height="1" width="1" style="display:none"
 			src="https://www.facebook.com/tr?id=1334935930899015&ev=PageView&noscript=1" /></noscript>
 	<!-- End Meta Pixel Code -->
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-72WJBXT4SV"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-72WJBXT4SV');
+	</script>
 	<?php wp_head(); ?>
 </head>
 
